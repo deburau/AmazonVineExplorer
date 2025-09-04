e3a285165881c3d0006f0edc7f0116975c9c55f6
deburau <deburau@users.noreply.github.com>
2025-09-05 00:03:12 +0200

    gotify click url

--- VineExplorer.user.js
+++ VineExplorer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon Vine Explorer
 // @namespace    https://github.com/deburau/AmazonVineExplorer
-// @version      0.11.25.9
+// @version      0.11.25.10
 // @updateURL    https://raw.githubusercontent.com/deburau/AmazonVineExplorer/main/VineExplorer.user.js
 // @downloadURL  https://raw.githubusercontent.com/deburau/AmazonVineExplorer/main/VineExplorer.user.js
 // @supportURL   https://github.com/deburau/AmazonVineExplorer/issues
@@ -2757,7 +2757,7 @@ function updateNewProductsBtn() {
                                         "contentType": "text/markdown"
                                     },
                                     'client::notification': {
-                                        click: window.location.origin + _prod.link,
+                                        click: { url: window.location.origin + _prod.link },
                                         bigImageUrl: fixProductImageUrl(_prod.data_img_url)
                                     }
                                 }
